6ede5a48a27ca2b2ad9a5adb6ccfb32f50e08cc7
Arrozado <149801163+Arrozado@users.noreply.github.com>
2023-11-04 10:24:36 -0400

    Update 1.tsx

--- src/pages/manga/1.tsx
+++ src/pages/manga/1.tsx
@@ -1,1 +1,28 @@
 
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Banner from '../../components/Banner'
+import Header from '../../components/Header'
+
+// interface for mangaScreen 
+interface MangaScreenProps {
+
+}
+
+function MangaScreen(props: MangaScreenProps) {
+    const router = useRouter()
+    const { id } = router.query
+
+    console.log("router", router)
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <h1>Manga {id}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default MangaScreen
