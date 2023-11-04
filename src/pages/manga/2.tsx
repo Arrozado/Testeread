9b32e561751dab57483c8ee4ac77e47442af3935
Arrozado <149801163+Arrozado@users.noreply.github.com>
2023-11-04 10:43:06 -0400

    Create 2.tsx

---
+++ src/pages/manga/2.tsx
@@ -0,0 +1,40 @@
+// MangaScreen.tsx
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Banner from '../../components/Banner';
+import Header from '../../components/Header';
+import Manga from '../../components/Manga'; // Importe o componente MangaCover aqui
+
+// interface for mangaScreen
+interface MangaScreenProps {}
+
+function MangaScreen(props: MangaScreenProps) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  console.log('router', router);
+
+  // Suponhamos que você tenha os dados do mangá, incluindo título, URL da capa e sinopse
+  const mangaData = {
+    title: 'Sexo',
+    imageUrl: 'https://img.lermanga.org/S/seoul-yeog-deulu-ideu-seoul-station-druid/capa.jpg',
+    synopsis: 'Sinopse do Mangá...',
+  };
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Manga {id}</h1>
+        {/* Renderize o componente MangaCover com os dados do mangá */}
+        <MangaCover
+          title={mangaData.title}
+          imageUrl={mangaData.imageUrl}
+          synopsis={mangaData.synopsis}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MangaScreen;
