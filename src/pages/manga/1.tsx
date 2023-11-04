0e977b5c341fc90f4d65ed212332758f8cb2c76e
Arrozado <149801163+Arrozado@users.noreply.github.com>
2023-11-04 10:32:42 -0400

    Update 1.tsx

--- src/pages/manga/1.tsx
+++ src/pages/manga/1.tsx
@@ -1,28 +1,40 @@
+// MangaScreen.tsx
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Banner from '../../components/Banner';
+import Header from '../../components/Header';
+import MangaCover from '../../components/Manga'; // Importe o componente MangaCover aqui
 
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import Banner from '../../components/Banner'
-import Header from '../../components/Header'
-
-// interface for mangaScreen 
-interface MangaScreenProps {
-
-}
+// interface for mangaScreen
+interface MangaScreenProps {}
 
 function MangaScreen(props: MangaScreenProps) {
-    const router = useRouter()
-    const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
 
-    console.log("router", router)
+  console.log('router', router);
 
-    return (
-        <div>
-            <Header />
-            <div>
-                <h1>Manga {id}</h1>
-            </div>
-        </div>
-    )
+  // Suponhamos que você tenha os dados do mangá, incluindo título, URL da capa e sinopse
+  const mangaData = {
+    title: 'Nome do Mangá',
+    imageUrl: 'URL_da_capa.jpg',
+    synopsis: 'Sinopse do Mangá...',
+  };
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Manga {id}</h1>
+        {/* Renderize o componente MangaCover com os dados do mangá */}
+        <MangaCover
+          title={mangaData.title}
+          imageUrl={mangaData.imageUrl}
+          synopsis={mangaData.synopsis}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default MangaScreen
+export default MangaScreen;
